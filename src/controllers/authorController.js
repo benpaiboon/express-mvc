c3b01d84dab278c3a3efd7ceb74efa8178e47154
unknown <paiboon@setscope.com>
2018-12-09 21:05:36 +0700

    change controller function style

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -2,53 +2,45 @@
 const Author = require('../models/authorModel');
 
 // Route Functions
-const getAuthors = async (req, res) => {
-  try {
-    res.send(await Author.find({}));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+const authorController = {
+  getAuthors: async (req, res) => {
+    try {
+      res.send(await Author.find({}));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  getAuthorById: async (req, res) => {
+    try {
+      res.send(await Author.findById({ _id: req.params.id }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  createAuthor: async (req, res) => {
+    try {
+      res.send(await Author.create(req.body));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  updateAuthorById: async (req, res) => {
+    try {
+      res.send(await Author.findByIdAndUpdate({ _id: req.params.id }, req.body, (error) => {
+        if (error) res.status(500).json({ error: error.message });
+        Author.findOne({ _id: req.params.id })
+      }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  removeAuthorById: async (req, res) => {
+    try {
+      res.send(await Author.findByIdAndRemove({ _id: req.params.id }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
-const getAuthorById = async (req, res) => {
-  try {
-    res.send(await Author.findById({ _id: req.params.id }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const createAuthor = async (req, res) => {
-  try {
-    res.send(await Author.create(req.body));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const updateAuthorById = async (req, res) => {
-  try {
-    res.send(await Author.findByIdAndUpdate({ _id: req.params.id }, req.body, (error) => {
-      if (error) res.status(500).json({ error: error.message });
-      Author.findOne({ _id: req.params.id })
-    }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const removeAuthorById = async (req, res) => {
-  try {
-    res.send(await Author.findByIdAndRemove({ _id: req.params.id }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-module.exports = {
-  getAuthors,
-  getAuthorById,
-  createAuthor,
-  updateAuthorById,
-  removeAuthorById
-}
+module.exports = authorController;
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,54 +1,46 @@
 // Init Book Model
 const Book = require('../models/bookModel');
 
-// Route Functions
-const getBooks = async (req, res) => {
-  try {
-    res.send(await Book.find({}));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+// Routes Function
+const bookController = {
+  getBooks: async (req, res) => {
+    try {
+      res.send(await Book.find({}));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  getBookById: async (req, res) => {
+    try {
+      res.send(await Book.findById({ _id: req.params.id }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  createBook: async (req, res) => {
+    try {
+      res.send(await Book.create(req.body));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  updateBookById: async (req, res) => {
+    try {
+      res.send(await Book.findByIdAndUpdate({ _id: req.params.id }, req.body, (error) => {
+        if (error) res.status(500).json({ error: error.message });
+        Book.findOne({ _id: req.params.id })
+      }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  removeBookById: async (req, res) => {
+    try {
+      res.send(await Book.findByIdAndRemove({ _id: req.params.id }));
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
-const getBookById = async (req, res) => {
-  try {
-    res.send(await Book.findById({ _id: req.params.id }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const createBook = async (req, res) => {
-  try {
-    res.send(await Book.create(req.body));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const updateBookById = async (req, res) => {
-  try {
-    res.send(await Book.findByIdAndUpdate({ _id: req.params.id }, req.body, (error) => {
-      if (error) res.status(500).json({ error: error.message });
-      Book.findOne({ _id: req.params.id })
-    }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const removeBookById = async (req, res) => {
-  try {
-    res.send(await Book.findByIdAndRemove({ _id: req.params.id }));
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-module.exports = {
-  getBooks,
-  getBookById,
-  createBook,
-  updateBookById,
-  removeBookById
-}
+module.exports = bookController;
